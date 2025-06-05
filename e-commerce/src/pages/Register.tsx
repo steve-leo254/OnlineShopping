009fd4482b9af05affcf12dfc5739638014181cf
wycliffe <ericoochieng456@gmail.com>
2025-06-05 09:11:58 +0300

    Enhance layout consistency in Register component

--- e-commerce/src/pages/Register.tsx
+++ e-commerce/src/pages/Register.tsx
@@ -81,8 +81,8 @@ const Register: React.FC = () => {
 
   return (
     <>
-      <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen lg:py-0">
           <a
             href="#"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
