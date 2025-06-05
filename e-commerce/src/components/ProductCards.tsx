5eebea1ad4e1652a69d088ffcf2a05059d4a24de
wycliffe <ericoochieng456@gmail.com>
2025-06-05 16:47:43 +0300

    Update discount badge color in ProductCards component

--- e-commerce/src/components/ProductCards.tsx
+++ e-commerce/src/components/ProductCards.tsx
@@ -72,7 +72,7 @@ const ProductCards: React.FC<ProductCardsProps> = ({ searchTerm, currentPage, se
                   </div>
                   <div className="pt-3">
                     <div className="mb-2 flex items-center justify-between gap-2">
-                      <span className="bg-blue-300 me-1 rounded bg-primary-100 px-1.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">
+                      <span className="bg-blue-400 me-1 rounded bg-primary-100 px-1.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">
                         Up to 35% off
                       </span>
                       <div className="flex items-center justify-end gap-1">
