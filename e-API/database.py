4a2342bfe98c3e109099caa0c5c0afc0326e7380
wycliffe <ericoochieng456@gmail.com>
2025-06-09 13:25:12 +0300

    feat: Update database connection string to use environment variable for database name

--- e-API/database.py
+++ e-API/database.py
@@ -10,7 +10,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 password = os.getenv("DB_PASSWORD")
-URL_DATABASE = f"mysql+pymysql://root:{password}@localhost:3306/ecommerce"
+db_name = os.getenv("DB_NAME", "react-pos")
+URL_DATABASE = f"mysql+pymysql://root:{password}@localhost:3306/{db_name}"
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
