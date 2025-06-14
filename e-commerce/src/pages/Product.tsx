2ebd4cd48cc618e0685ffd4e5b383b1725a5a708
wycliffe <ericoochieng456@gmail.com>
2025-06-15 00:21:55 +0300

    Remove unused grandTotal calculation and clean up Products component by eliminating fetch logic

--- e-commerce/src/pages/Product.tsx
+++ e-commerce/src/pages/Product.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { useFetchProducts } from "../components/UseFetchProducts";
-import { useEffect } from "react";
 import ProductsTable from "../components/ProductsTable";
 
 const Products: React.FC = () => {
-  const { products, fetchProducts } = useFetchProducts();
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+ 
 
   return (
     <>
