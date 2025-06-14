0185f6044974c227a7540b267f4c96c4a5c09d01
steve-leo254 <steveleo254@gmail.com>
2025-06-14 14:34:58 -0700

    commit

--- e-commerce/src/components/getActionDropdown.tsx
+++
@@ -1,7 +0,0 @@
-import OrderActionDropdown from './OrderActionDropdown';
-
-const getActionDropdown = (status: string, orderId: number, onCancel: () => void) => {
-  return OrderActionDropdown({ status, orderId, onCancel });
-};
-
-export default getActionDropdown;
